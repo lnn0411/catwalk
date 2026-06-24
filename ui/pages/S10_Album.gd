2cce3349b32273c11008e6ff2dab87cbef1c3614
库洛洛 <art_agent@hermes.dev>
2026-06-24 15:49:47 +0800

    restore: S10_Album完整色块版(从commit 21b10b5恢复)

--- ui/pages/S10_Album.gd
+++ ui/pages/S10_Album.gd
@@ -7,22 +7,6 @@ var _current_tab := Tab.CATS
 func _ready() -> void:
 	super._ready()
 	_switch_tab(Tab.CATS)
-	_setup_cards()
-
-func _setup_cards() -> void:
-	var grid := $VBox/Body/CatsGrid
-	for i in grid.get_child_count():
-		var card := grid.get_child(i) as ColorRect
-		if card:
-			var btn := Button.new()
-			btn.flat = true
-			btn.size = Vector2(160, 180)
-			btn.pressed.connect(func(): _on_card_pressed(i))
-			card.add_child(btn)
-
-func _on_card_pressed(index: int) -> void:
-	var data := {"name": "猫咪%d" % (index + 1), "breed": "普通", "level": 4 - index}
-	UIManager.replace("res://ui/pages/S10_CatDetail.tscn", data)
 
 func _switch_tab(tab: Tab) -> void:
 	_current_tab = tab
@@ -40,4 +24,4 @@ func _on_tab_cards_pressed() -> void: _switch_tab(Tab.CARDS)
 func _on_tab_ach_pressed() -> void: _switch_tab(Tab.ACH)
 
 func _on_back_pressed() -> void:
-	UIManager.replace("res://scenes/S04_GardenMain.tscn")
+	back_requested.emit()
